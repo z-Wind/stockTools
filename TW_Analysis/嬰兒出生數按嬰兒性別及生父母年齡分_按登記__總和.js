170da26cc317289444ce2bff7a65886bd192f49c
z-Wind <z-Wind@users.noreply.github.com>
2025-05-08 23:39:21 +0000

    deploy: c53f18976d3b240585b8c7c54290a6e93e0ada25

---
+++ TW_Analysis/嬰兒出生數按嬰兒性別及生父母年齡分_按登記__總和.js
@@ -0,0 +1,8 @@
+var config = { responsive: true }
+
+var 嬰兒出生數按嬰兒性別及生父母年齡分_按登記__總和 = {"data": [{"type": "scatter", "name": "birth_count", "x": ["110", "111", "112", "113"], "y": [153810, 138972, 135558, 134844], "mode": "lines"}], "layout": {"title": {"text": "\u5b30\u5152\u51fa\u751f\u6578\u6309\u5b30\u5152\u6027\u5225\u53ca\u751f\u7236\u6bcd\u5e74\u9f61\u5206_\u6309\u767b\u8a18__\u7e3d\u548c 110~113", "font": {"family": "Times New Roman"}, "x": 0.05, "y": 0.9}, "hovermode": "x", "xaxis": {"type": "category", "tickfont": {"family": "Courier New", "size": 14}, "automargin": true}, "font": {"family": "Courier New", "color": "#ffffff"}, "yaxis": {"tickfont": {"family": "Courier New"}, "automargin": true}, "plot_bgcolor": "#000", "paper_bgcolor": "#000"}};
+
+Plotly.newPlot("嬰兒出生數按嬰兒性別及生父母年齡分_按登記__總和",
+    嬰兒出生數按嬰兒性別及生父母年齡分_按登記__總和.data,
+    嬰兒出生數按嬰兒性別及生父母年齡分_按登記__總和.layout || {},
+    config);
---
+++ TW_Analysis/離婚_終止結婚人數按婚姻類型_性別_年齡_原屬國籍_地區_及教育程度分_按登記__原屬國籍.js
@@ -0,0 +1,8 @@
+var config = { responsive: true }
+
+var 離婚_終止結婚人數按婚姻類型_性別_年齡_原屬國籍_地區_及教育程度分_按登記__原屬國籍 = {"data": [{"type": "scatter", "name": "\u672c\u570b", "x": ["111", "112", "113"], "y": [94282, 97763, 98912], "mode": "lines"}, {"type": "scatter", "name": "\u5927\u9678\u5730\u5340", "x": ["111", "112", "113"], "y": [2994, 3950, 3626], "mode": "lines"}, {"type": "scatter", "name": "\u6e2f\u6fb3\u5730\u5340", "x": ["111", "112", "113"], "y": [230, 363, 312], "mode": "lines"}, {"type": "scatter", "name": "\u6771\u5357\u4e9e\u5730\u5340", "x": ["111", "112", "113"], "y": [3111, 3267, 3254], "mode": "lines"}, {"type": "scatter", "name": "\u5176\u4ed6\u5730\u5340", "x": ["111", "112", "113"], "y": [601, 827, 834], "mode": "lines"}], "layout": {"title": {"text": "\u96e2\u5a5a_\u7d42\u6b62\u7d50\u5a5a\u4eba\u6578\u6309\u5a5a\u59fb\u985e\u578b_\u6027\u5225_\u5e74\u9f61_\u539f\u5c6c\u570b\u7c4d_\u5730\u5340_\u53ca\u6559\u80b2\u7a0b\u5ea6\u5206_\u6309\u767b\u8a18__\u539f\u5c6c\u570b\u7c4d 111~113", "font": {"family": "Times New Roman"}, "x": 0.05, "y": 0.9}, "hovermode": "x", "xaxis": {"type": "category", "tickfont": {"family": "Courier New", "size": 14}, "automargin": true}, "font": {"family": "Courier New", "color": "#ffffff"}, "yaxis": {"tickfont": {"family": "Courier New"}, "automargin": true}, "plot_bgcolor": "#000", "paper_bgcolor": "#000"}};
+
+Plotly.newPlot("離婚_終止結婚人數按婚姻類型_性別_年齡_原屬國籍_地區_及教育程度分_按登記__原屬國籍",
+    離婚_終止結婚人數按婚姻類型_性別_年齡_原屬國籍_地區_及教育程度分_按登記__原屬國籍.data,
+    離婚_終止結婚人數按婚姻類型_性別_年齡_原屬國籍_地區_及教育程度分_按登記__原屬國籍.layout || {},
+    config);

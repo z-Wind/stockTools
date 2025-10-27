46a0aa2eb8a0ef0554eb6eda64e3289203b20327
z-Wind <zpsyhapcst@gmail.com>
2025-10-27 15:06:06 +0800

    food 加入細項資料

--- tw_food.py
+++ tw_food.py
@@ -1,8 +1,10 @@
+import json
 import io
 import zipfile
 import pandas as pd
-import requests
+import plotly
 import minify_html
+import requests
 
 from flask import Flask, render_template
 from pathlib import Path
@@ -10,6 +12,29 @@ from pathlib import Path
 from tw_analysis_data import sanitize_filename
 
 app = Flask(__name__)
+
+
+def _mergeDict(a, b, path=None, overwrite=True):
+    """Merges b into a. If overwrite is True, b's values will overwrite a's on conflict."""
+    if path is None:
+        path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                _mergeDict(a[key], b[key], path + [str(key)], overwrite)
+            elif isinstance(a[key], list) and isinstance(b[key], list):
+                a[key] += b[key]
+            elif a[key] != b[key] and overwrite:
+                a[key] = b[key]
+            elif a[key] == b[key]:
+                pass  # same leaf value
+            elif not overwrite:
+                print(
+                    f"Conflict at {'.'.join(path + [str(key)])} and overwrite is False. Keeping original value."
+                )
+        else:
+            a[key] = b[key]
+    return a
 
 
 def main():
@@ -41,6 +66,61 @@ def main():
         subreport_folder = report_dir / prefix
         subreport_folder.mkdir(parents=True, exist_ok=True)
 
+        detail_links = {}
+        for idx, row in df.iterrows():
+            detail_folder = subreport_folder / "Detail"
+            detail_folder.mkdir(parents=True, exist_ok=True)
+
+            row = row.to_frame().reset_index(level=2)
+            df_含量 = row[row["level_2"] != "排名"]
+            df_排名 = row[row["level_2"] == "排名"][idx]
+            df_detail = pd.concat([df_含量, df_排名], axis="columns")
+            df_detail.columns = pd.MultiIndex.from_product(
+                [[i] for i in idx] + [["單位", "含量", "排名"]]
+            )
+
+            dataList = []
+            for i in df_detail.index:
+                name = f"{i[0]}_{i[1]}"
+                data = [int(str.split(df_detail.loc[i, idx + ("排名",)], "/")[0])]
+                dataList.append({"type": "bar", "name": name, "x": ["排名"], "y": data})
+
+            layout = {
+                "title": {"text": f"{idx}"},
+                "hovermode": "x",
+                "yaxis": {
+                    "autorange": "reversed",
+                },
+                "barmode": "group",
+            }
+
+            graph = {"data": dataList, "layout": layout, "config": {}}
+            graph = _mergeDict(plotly.io.templates["plotly_dark"].to_plotly_json(), graph)
+
+            html = render_template(
+                "tw_food.html.j2",
+                links={},
+                tables={},
+                detail=df_detail.to_html(
+                    classes=["table", "table-dark", "table-striped", "table-hover"],
+                ),
+                graph=json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder),
+                title=f"{"_".join(idx)} Report",
+            )
+            filename = f"{sanitize_filename("_".join(idx))}_Report.html"
+            with open(detail_folder / f"{filename}", "w", encoding="UTF-8") as f:
+                minified_html = minify_html.minify(
+                    html, keep_comments=False, keep_html_and_head_opening_tags=False
+                )
+                f.write(minified_html)
+
+            detail_links[idx] = f"./Detail/{filename}"
+
+        df["細項資料"] = [
+            f"<a href='{detail_links[idx]}' target='_blank'>連結</a>" for idx in df.index
+        ]
+        df = df.set_index("細項資料", append=True)
+
         links = {}
         for 分析項分類, 分析項, 含量單位 in 營養素種類:
             col = (分析項分類, 分析項, 含量單位)
@@ -69,6 +149,8 @@ def main():
             tables = {
                 "全部": df_全部.to_html(
                     classes=["table", "table-dark", "table-striped", "table-hover"],
+                    escape=False,
+                    render_links=True,
                 )
             }
 
@@ -77,6 +159,8 @@ def main():
                 df_食品大類.index += 1
                 tables[食品大類] = df_食品大類.to_html(
                     classes=["table", "table-dark", "table-striped", "table-hover"],
+                    escape=False,
+                    render_links=True,
                 )
 
             html = render_template(
